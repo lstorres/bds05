99f364999f96ae883893ed25dd7ddd9cc12377bf
Lucas_Torres <lstorres093@gmail.com>
2021-06-15 19:39:58 -0300

    Fix in Genre id

--- src/main/java/com/devsuperior/movieflix/entities/Genre.java
+++ src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -17,6 +20,8 @@ public class Genre implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
